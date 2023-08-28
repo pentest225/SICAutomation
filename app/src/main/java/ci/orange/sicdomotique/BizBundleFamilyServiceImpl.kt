167d0bb93c8d792e489d95111a0317fe48e45eb7
Patrick Dagouaga <patrickdagouaga@gmail.com>
2023-08-28 12:22:11 +0000

    Intregration des fichier des configuration du UIBizzBundle et compilation ok

---
+++ app/src/main/java/ci/orange/sicdomotique/BizBundleFamilyServiceImpl.kt
@@ -0,0 +1,18 @@
+package ci.orange.sicdomotique
+
+import com.thingclips.smart.commonbiz.bizbundle.family.api.AbsBizBundleFamilyService
+
+class BizBundleFamilyServiceImpl : AbsBizBundleFamilyService() {
+    private var mHomeId: Long = 0
+    private var mHomeName: String? = null
+
+    override fun getCurrentHomeId(): Long {
+        return mHomeId
+    }
+
+    override fun shiftCurrentFamily(familyId: Long, curName: String?) {
+        super.shiftCurrentFamily(familyId, curName)
+        mHomeId = familyId
+        mHomeName = curName
+    }
+}
